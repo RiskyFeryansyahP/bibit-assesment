caeb9ecbb2a06c9df1a778ec85e0b4e14abb6f94
Risky Feryansyah Pribadi <riskypribadi24@gmail.com>
2021-09-07 22:05:39 +0700

    chore: init project structure for movies service

---
+++ Soal 2/movies/internal/handler/movie.go
@@ -0,0 +1,39 @@
+package handler
+
+import (
+	"context"
+
+	"github.com/RiskyFeryansyahP/bibit-movies/internal/model"
+	"google.golang.org/grpc"
+)
+
+// MovieHandler ...
+type MovieHandler struct {
+	MovieUC model.UsecaseMovie
+}
+
+// NewMovieHandler ...
+func NewMovieHandler(server *grpc.Server, movieUC model.UsecaseMovie) {
+	handler := &MovieHandler{
+		MovieUC: movieUC,
+	}
+
+	model.RegisterMovieServiceServer(server, handler)
+}
+
+// SearchMovie ...
+func (mh *MovieHandler) SearchMovie(ctx context.Context, req *model.RequestSearchMovies) (*model.ResponseSearchMovies, error) {
+	keyword := req.Searchword
+	page := req.Pagination
+
+	movies, err := mh.MovieUC.SearchMovie(ctx, keyword, page)
+	if err != nil {
+		return nil, err
+	}
+
+	resp := &model.ResponseSearchMovies{
+		Movies: movies,
+	}
+
+	return resp, nil
+}
---
+++ Soal 2/movies/internal/repository/movie.go
@@ -0,0 +1,40 @@
+package repository
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+
+	"github.com/RiskyFeryansyahP/bibit-movies/internal/model"
+)
+
+// MovieRepository ...
+type MovieRepository struct{}
+
+// NewMovieRepository ...
+func NewMovieRepository() model.RepositoryMovie {
+	return &MovieRepository{}
+}
+
+// Search ...
+func (mr *MovieRepository) Search(ctx context.Context, keyword, page string) ([]*model.Movie, error) {
+	var movies []*model.Movie
+
+	url := fmt.Sprintf("http://www.omdbapi.com/?apikey=faf7e5bb&s=%s&page=%s", keyword, page)
+
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+
+	b, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	_ = json.Unmarshal(b, &movies)
+
+	return movies, nil
+}
